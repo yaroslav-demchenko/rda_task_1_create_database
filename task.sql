b5831c732d2f19fd05e1c161c36973761e7346c1
yaroslav-demchenko <dastex1411@gmail.com>
2026-04-03 13:43:13 +0300

    shopdb database creation

--- task.sql
+++ task.sql
@@ -1,1 +1,36 @@
-# Write your SQL code for the database creation here. Good luck! 
+CREATE database ShopDB;
+USE ShopDB;
+
+
+CREATE TABLE Products (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    Description VARCHAR(100),
+    Price INT NOT NULL,
+    WarehouseAmount INT,
+    PRIMARY KEY (ID)
+)
+
+CREATE TABLE Customers (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    Address VARCHAR(100)
+)
+
+CREATE TABLE Orders (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    CustomerID INT,
+    Date Date,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
+)
+
+CREATE TABLE OrderItems (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    OrderID INT,
+    ProductID INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
+)
+
